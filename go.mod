25b3b618402474376d287b7a19b38d7fa812f614
Onurhan Ak <onurhanak@protonmail.com>
2024-08-22 10:38:06 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,51 @@
+module libgenScraper
+
+go 1.22.6
+
+require (
+	github.com/charmbracelet/bubbles v0.19.0
+	github.com/charmbracelet/bubbletea v0.27.0
+	github.com/gocolly/colly v1.2.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.2 // indirect
+	github.com/antchfx/xmlquery v1.4.1 // indirect
+	github.com/antchfx/xpath v1.3.1 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
+	github.com/charmbracelet/lipgloss v0.13.0 // indirect
+	github.com/charmbracelet/x/ansi v0.2.3 // indirect
+	github.com/charmbracelet/x/input v0.2.0 // indirect
+	github.com/charmbracelet/x/term v0.2.0 // indirect
+	github.com/charmbracelet/x/windows v0.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	colly "github.com/gocolly/colly"
+)
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type Book struct {
+	ID, Title, Author, Link, Filetype, MD5 string
+}
+
+type model struct {
+	textInput textinput.Model
+	table     table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "enter":
+			getBook(m.textInput.Value())
+
+		case "d":
+			go DownloadFile(m.table.SelectedRow()[1], m.table.SelectedRow()[2], m.table.SelectedRow()[3])
+			return m, tea.Batch(
+				tea.Printf("Downloading %s", m.table.SelectedRow()[1]),
+			)
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	m.textInput, cmd = m.textInput.Update(msg)
+	m.table, cmd = m.table.Update(msg)
+
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.textInput.View()) + "\n" + baseStyle.Render(m.table.View()) + "\n"
+}
+
+func strip(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') ||
+			b == ' ' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
+}
+
+func getBook(query string) {
+	c := colly.NewCollector(
+		colly.AllowedDomains("libgen.is"),
+	)
+
+	var books []Book
+
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		book := Book{}
+
+		e.ForEach("td", func(index int, el *colly.HTMLElement) {
+			switch index {
+			case 0:
+				book.ID = el.Text
+			case 1:
+				book.Author = el.Text
+			case 8:
+				book.Filetype = el.Text
+			case 2:
+				book.Title = el.ChildText("a")
+				md5 := strings.Split(el.ChildAttr("a", "href"), "md5=")
+				if len(md5) == 2 {
+					book.MD5 = strings.Split(el.ChildAttr("a", "href"), "md5=")[1]
+				} else {
+					book.MD5 = ""
+				}
+
+			}
+		})
+
+		if book.Title != "" {
+			books = append(books, book)
+		}
+	})
+
+	query = url.QueryEscape(query)
+	var targetPage string = "https://libgen.is/search.php?req=" + query + "&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def"
+
+	err := c.Visit(targetPage)
+	if err != nil {
+		log.Println("Failed to visit target page:", err)
+		return
+	}
+
+	p := tea.NewProgram(bookSearchModel(books))
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generateDownloadLink(md5 string, bookID string, bookTitle string, bookFiletype string) string {
+	var newBookID string
+	if len(bookID) == 4 {
+		newBookID = string(bookID[:1]) + "000"
+	} else if len(bookID) == 5 {
+		newBookID = string(bookID[:2]) + "000"
+	}
+
+	md5 = strings.ToLower(md5)
+	bookTitle = strings.Replace(strip(bookTitle), " ", "_", -1)
+	downloadLink := "https://download.library.lol/main/" + newBookID + "/" + md5 + "/" + bookTitle + "." + bookFiletype
+
+	return downloadLink
+}
+
+func DownloadFile(title string, filetype string, link string) error {
+	fileName := fmt.Sprintf("%s.%s", title, filetype)
+	out, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Download finished for %s", title)
+	return nil
+}
+
+func bookSearchModel(books []Book) model {
+	fmt.Print("\033[H\033[2J")
+	columns := []table.Column{
+		{Title: "Authors", Width: 30},
+		{Title: "Title", Width: 60},
+		{Title: "Filetype", Width: 10},
+		{Title: "Link", Width: 30},
+	}
+
+	rows := []table.Row{}
+
+	for index, book := range books {
+		if index < 3 {
+			continue
+		} else if index == 50 {
+			break
+		}
+
+		book.Link = generateDownloadLink(book.MD5, book.ID, book.Title, book.Filetype)
+		rows = append(rows, []string{book.Author, book.Title, book.Filetype, book.Link})
+	}
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(15),
+	)
+
+	ti := textinput.New()
+	ti.Placeholder = "Query"
+	ti.Focus()
+	ti.CharLimit = 250
+	ti.Width = 135
+
+	m := model{ti, t}
+
+	return m
+}
+
+func main() {
+	fmt.Print("\033[H\033[2J")
+	columns := []table.Column{
+		{Title: "Authors", Width: 30},
+		{Title: "Title", Width: 60},
+		{Title: "Filetype", Width: 10},
+		{Title: "Link", Width: 30},
+	}
+
+	rows := []table.Row{}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(0),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	ti := textinput.New()
+	ti.Placeholder = "Query"
+	ti.Focus()
+	ti.CharLimit = 250
+	ti.Width = 135
+	fmt.Println("Enter to search. Q to quit. D to download.")
+	m := model{ti, t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+}
